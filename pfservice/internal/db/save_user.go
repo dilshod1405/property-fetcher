8d16736a3011b89782fcc635281e9e162416f3e7
dilshod1405 <dilshod.normurodov1392@gmail.com>
2026-01-05 20:02:08 +0500

    updated

--- pfservice/internal/db/save_user.go
+++ pfservice/internal/db/save_user.go
@@ -3,27 +3,45 @@ package db
 import (
 	"errors"
 	"pfservice/internal/users"
-
+	"strings"
 	"gorm.io/gorm"
 )
+
+
+func normalizeAvatar(avatar string) string {
+	if avatar == "" {
+		return ""
+	}
+
+	if strings.HasPrefix(avatar, "http://") || strings.HasPrefix(avatar, "https://") {
+		return ""
+	}
+
+	return avatar
+}
+
 
 func SaveOrUpdateUser(db *gorm.DB, u users.DjangoUser) (users.DjangoUser, error) {
 	var existing users.DjangoUser
 
 	err := db.Where("email = ?", u.Email).First(&existing).Error
 	if err == nil {
-		// Update existing
 		u.ID = existing.ID
 		u.CreatedAt = existing.CreatedAt
+
+		u.Avatar = normalizeAvatar(u.Avatar)
+
 		return u, db.Save(&u).Error
 	}
+
 
 	if !errors.Is(err, gorm.ErrRecordNotFound) {
 		return u, err
 	}
 
-	// Create new
+	u.Avatar = normalizeAvatar(u.Avatar)
 	return u, db.Create(&u).Error
+
 }
 
 
